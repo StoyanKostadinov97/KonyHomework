88bca9e3fd32bf0f3a6099368f5d8701e5787505
StoyanKostadinov97 <stoyanKostadinov@outlook.com>
2021-07-03 16:37:46 +0300

    -

---
+++ controllers/desktop/Form1Controller/Form1ControllerActions.js
@@ -0,0 +1,17 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for btnNavComponent **/
+    AS_Button_ce6aeaf5629b4b408ef3e0f9775a568c: function AS_Button_ce6aeaf5629b4b408ef3e0f9775a568c(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("Form2");
+        ntf.navigate();
+    },
+    /** onClick defined for btnImgComponent **/
+    AS_Button_af6db8b892f348d1bd1481d4c755ce68: function AS_Button_af6db8b892f348d1bd1481d4c755ce68(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("Form2");
+        ntf.navigate();
+    }
+});
